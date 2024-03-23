7780894ee4f1beac2dfeb2190a543fb73ff3cb9e
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-03-23 08:33:12 +0530

    Linked List performance with parallel streams

---
+++ src/test/java/com/learnjava/parallelstreams/LinkedListSpliteratorExampleTest.java
@@ -0,0 +1,40 @@
+package com.learnjava.parallelstreams;
+
+import com.learnjava.util.DataSet;
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListSpliteratorExampleTest {
+
+    LinkedListSpliteratorExample linkedListSpliteratorExample = new LinkedListSpliteratorExample();
+
+    @RepeatedTest(5)
+        // repeated test is an annotation where the same test will be executed specified number of times.
+        // idea behind is when the load increases on the machine, it tries to use the cache,
+        // it may result in performance improvement, what would be the best result possible.
+    void arrayListSeparatorExample_sequential() {
+
+        int size = 1000000;
+        LinkedList<Integer> inputList = DataSet.generateIntegerLinkedList(size);
+
+        List<Integer> resultList = linkedListSpliteratorExample.multiplyEachValue(inputList, 2, false);
+
+        assertEquals(size, resultList.size());
+    }
+
+    @RepeatedTest(5)
+    void arrayListSeparatorExample_parallel() {
+
+        int size = 1000000;
+        LinkedList<Integer> inputList = DataSet.generateIntegerLinkedList(size);
+
+        List<Integer> resultList = linkedListSpliteratorExample.multiplyEachValue(inputList, 2, true);
+
+        assertEquals(size, resultList.size());
+    }
+
+}
